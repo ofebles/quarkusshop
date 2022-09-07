8678cfcf93cfe4e7e3b0554a6a313183c7501949
Orestes Febles <ofebles@gmail.com>
2022-09-08 00:08:44 +0100

    First commit

---
+++ src/main/java/org/acme/entities/Cart.java
@@ -0,0 +1,52 @@
+package org.acme.entities;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.acme.enums.CartStatus;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@Entity
+@Table(name = "cart")
+public class Cart extends AbstractEntity {
+
+    @ManyToOne
+    private Customer customer;
+
+    @NotNull
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CartStatus status;
+
+    public Cart(Customer customer, @NotNull CartStatus status) {
+        this.customer = customer;
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cart cart = (Cart) o;
+
+        return Objects.equals(customer, cart.customer) && status == cart.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customer, status);
+    }
+
+}
---
+++ src/main/java/org/acme/repository/CartRepository.java
@@ -0,0 +1,16 @@
+package org.acme.repository;
+
+import org.acme.entities.Cart;
+import org.acme.enums.CartStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    List<Cart> findByStatus(CartStatus status);
+
+    List<Cart> findByStatusAndCustomerId(CartStatus status, Long customerId);
+}
